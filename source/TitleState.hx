d4708e379a4754348c2c317bd08059c20303b238
LionelPPAK <124942396+LionelPPAK@users.noreply.github.com>
2023-02-10 15:46:56 -0500

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -408,7 +408,7 @@ class TitleState extends MusicBeatState
 	}
 
 	var transitioning:Bool = false;
-	private static var playJingle:Bool = false;
+	//private static var playJingle:Bool = false;
 	
 	var newTitle:Bool = false;
 	var titleTimer:Float = 0;
